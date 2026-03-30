c9ac1603438d024e90f39508ba51e583ad2efab1
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-03-30 11:59:44 -0300

    feat: create static home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,28 @@
-function App() {
+import { BsCartPlus } from "react-icons/bs"
+
+function Home() {
 
   return (
-    <>
-      <h1 className="text-4xl">Projeto</h1>
-    </>
+    <main>
+        <div className="w-full max-w-7xl px-4 m-auto">
+            <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+                <section className="w-full rounded-lg max-h-70 mb-2">
+                    <img className="full" src="" alt="" />
+                    <p className="font-medium mt-1 mb-2">Título do produto</p>
+
+                    <div className="flex gap-3 items-center">
+                        <strong className="text-zinc-700/50">preço</strong>
+                        <button className="bg-zinc-900 p-1 rounded">
+                            <BsCartPlus size={15} color="#fff"/>
+                        </button>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </main>
   )
 }
 
-export default App
+export default Home
